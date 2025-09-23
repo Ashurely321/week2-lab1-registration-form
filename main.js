a44aeab226fea51506be17072dbb83340735892b
Ashley Harris <laylen24@gmail.com>
2025-09-23 10:22:24 -0500

    follow along with class to continue later

---
+++ main.js
@@ -0,0 +1,8 @@
+//HTML selectors
+const mainForm=dicument.getElementById("main-form");
+const outputDiv = document.getElementById("output");
+
+//Event Listener
+mainForm.addEventListener ("submit", function(event)) {
+event.preventDefault();
+}
